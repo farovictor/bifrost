95ea31976778894e4344debf50523d1edbecc1d6
Victor Faro <victor.faro@gmail.com>
2025-06-21 16:18:53 -0300

    Remove duplicate comment

--- pkg/services/service.go
+++ pkg/services/service.go
@@ -1,7 +1,6 @@
 package services
 
 // Service represents an upstream service to which requests can be proxied.
-// Service represents an upstream service to which requests can be proxied.
 type Service struct {
 	ID        string `json:"id"`
 	Endpoint  string `json:"endpoint"`
