807d9e02360797a9b1b081d5bce9ee0877795aa7
Victor Faro <victor.faro@gmail.com>
2025-08-11 08:01:21 -0300

    Drop postgres uuid defaults for sqlite

--- pkg/users/user.go
+++ pkg/users/user.go
@@ -2,7 +2,7 @@ package users
 
 // User represents an API user able to authenticate to Bifrost.
 type User struct {
-	ID     string `json:"id" gorm:"primaryKey;size:255;default:uuid_generate_v4()"`
+	ID     string `json:"id" gorm:"primaryKey;size:255"`
 	Name   string `json:"name" gorm:"not null"`
 	Email  string `json:"email" gorm:"not null;unique"`
 	APIKey string `json:"api_key" gorm:"not null"`
